aff02c34645a5078023df1657ec5924a8b46cc04
MaximilianLau <43243180+MaximilianLau@users.noreply.github.com>
2020-05-04 18:15:33 -0400

    Just splitting components into layout/header/footer and simple mockup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import Header from './components/header/Header.js'
+import Footer from './components/footer/Footer.js'
+import Form from './components/form/Form.js'
 import './App.css';
 
 
@@ -8,17 +11,21 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-
     }
   }
   
   render() {
     return (
-      <div className="App">
-      </div>
-    );
+      <div>
+        <div className="mainGrid">
+        <Header />
 
-    }
+        <Form />
+        </div>
+        <Footer />
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/form/Form.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+  render() {
+    return (
+      <div className="formContainer">
+        <h2>This is a Form</h2>
+      </div>
+    )
+  }
+}
+
+export default Form;
