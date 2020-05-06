1b38a78558d2cd2c720cbd9d6d0383c0038522e2
Edward Boland <mr.eboland@gmail.com>
2020-05-05 22:16:50 -0400

    basic user info form complete. Basic styling complete. Input functionality complete

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import About from "../../About";
 
 class Form extends Component {
+
   render() {
     return (
       <div className="formContainer">
         <h2>This is a Form</h2>
+        <About />
       </div>
     )
   }
