487ac4119fb64add43bb95dcfbf119735fd6ee83
Marie Erin <m.oshaughnessy@outlook.com>
2020-05-06 20:01:07 -0400

    created Preview component

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Preview from './Preview.js'
+
 
 class Form extends Component {
   render() {
     return (
       <div className="formContainer">
         <h2>This is a Form</h2>
+       <Preview />
       </div>
     )
   }
