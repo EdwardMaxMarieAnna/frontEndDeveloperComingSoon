22bd8a4a3d79d0c61ab07a9bbba9e66b2b7c59b4
Edward Boland <mr.eboland@gmail.com>
2020-09-13 19:40:26 -0400

    Updates

--- precache-manifest.3ac4c1b04acbf1c9d39a4bcfd37ea95f.js
+++ precache-manifest.3ac4c1b04acbf1c9d39a4bcfd37ea95f.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c6d1381e2f9a9582199e1d01a9b8a661",
+    "revision": "99a3c97f0a1c2d308e1248866e841292",
     "url": "/frontEndDeveloperComingSoon/index.html"
   },
   {
-    "revision": "2d2806979cbdf6f3b0fa",
+    "revision": "6d4cbd3c8bbdd92cc0f6",
     "url": "/frontEndDeveloperComingSoon/static/css/main.ac655ab2.chunk.css"
   },
   {
-    "revision": "6748bc8b4ca7149ad9b9",
-    "url": "/frontEndDeveloperComingSoon/static/js/2.a7b3a5a2.chunk.js"
+    "revision": "c36389fdaf9c72e0ccf8",
+    "url": "/frontEndDeveloperComingSoon/static/js/2.234cc644.chunk.js"
   },
   {
     "revision": "f9b3e87900b5f3843f2b97c9b5b24d1e",
-    "url": "/frontEndDeveloperComingSoon/static/js/2.a7b3a5a2.chunk.js.LICENSE.txt"
+    "url": "/frontEndDeveloperComingSoon/static/js/2.234cc644.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2d2806979cbdf6f3b0fa",
-    "url": "/frontEndDeveloperComingSoon/static/js/main.4dd6992e.chunk.js"
+    "revision": "6d4cbd3c8bbdd92cc0f6",
+    "url": "/frontEndDeveloperComingSoon/static/js/main.ede3b1e2.chunk.js"
   },
   {
     "revision": "1a1ebfa7c4901ffaa0af",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/frontEndDeveloperComingSoon/precache-manifest.c43a0d93aa612fddb09023415b2466ee.js"
+  "/frontEndDeveloperComingSoon/precache-manifest.3ac4c1b04acbf1c9d39a4bcfd37ea95f.js"
 );
 
 self.addEventListener('message', (event) => {
